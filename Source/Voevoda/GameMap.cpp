65d32d42f8c1b21ec4056f9f2f2ebed4162a0c4f
EkaterinaAB <134541902+EkaterinaAB@users.noreply.github.com>
2024-06-07 12:04:16 -0700

    Update GameMap.cpp

--- Source/Voevoda/GameMap.cpp
+++ Source/Voevoda/GameMap.cpp
@@ -87,47 +87,149 @@ void GameMap::random_woods_and_mountains() {
 }
 
 
+bool GameMap::IsValid(int32 XValue, int32 YValue) {
+	return (XValue >= 0 && XValue <= Width - 1 && YValue >= 0 && YValue <= Height - 1
+		&& TerrainData[XValue ][YValue ] == Grass);
+}
+
+void GameMap::dfs_for_random_river(int32 XValue, int32 YValue, TArray<TArray<int32>> &VisitedArray) {
+
+	VisitedArray[XValue][YValue] = 1;
+
+	TArray<int32> DirectionX = { 1, -1, 0, 0 };
+	TArray<int32> DirectionY = { 0,  0, 1, -1};
+
+
+	for (int32 i = 0; i < DirectionX.Num(); i++)
+	{
+		if (IsValid(XValue + DirectionX[i], YValue + DirectionY[i]) &&
+			!VisitedArray[XValue + DirectionX[i]][YValue + DirectionY[i]]) {
+			dfs_for_random_river(XValue+ DirectionX[i], YValue+ DirectionY[i], VisitedArray);
+		}
+	}
+}
+
 void GameMap::random_river() {
 
-	for (size_t count = 0; count < Width/20; count++)
+	for (size_t count = 0; count < Width / 15; count++)
 	{
 
 		int32 StartX = FMath::RandRange(10, Width - 10);
 		int32 StartY = FMath::RandRange(10, Height - 10);
 
-		int32 HeadwaterWidth = FMath::RandRange(3, 7);
+		int32 HeadwaterWidth = FMath::RandRange(3, 5);
+
+		bool waterNear = 0;
+
+		for (int32 i = 1; i <= HeadwaterWidth; i++)
+		{
+			for (int32 j = 0; j < i; j++)
+			{
+				if (TerrainData[StartX - i + 1 + j][StartY + i - 1] == Water) {
+					waterNear = 1;
+				}
+			}
+		}
+
+		if (waterNear) {
+			continue;
+		}
+
+		TArray <FIntPoint> riverSells;
 
 		for (int32 i = 1; i <= HeadwaterWidth; i++)
 		{
 			for (int32 j = 0; j < i; j++)
 			{
 				TerrainData[StartX - i + 1 + j][StartY + i - 1] = Water;
+				riverSells.Add({ StartX - i + 1 + j , StartY + i - 1 });
 			}
 		}
 
-		TArray<int32> DirectionX = {1,1,1, 1, 1, 1 ,1, 0, 1, 1, 1, 0, 1, -1, -1, 0 };
-		TArray<int32> DirectionY = {1,1,0,-1, 1, 1 ,0, 1, 1, 1, 0, 1, -1, 1, 0, -1 };
+		TArray<int32> DirectionX = { 1, 1, 1, 1, 1, 1 , 1, 0, 1, 1, 1, 0, 1, 1, 1, 0 };
+		TArray<int32> DirectionY = { 1, 1, 0, 1, 1, 1 , 0, 1, 1, 1, 0, 1, 1, 1, 0, 1 };
+
+		int32 DirectionXSign = FMath::RandRange(0, 1);
+		int32 DirectionYSign = FMath::RandRange(0, 1);
+
+		DirectionXSign = DirectionXSign ? 1 : -1;
+		DirectionYSign = DirectionYSign ? 1 : -1;
 
 		while (StartX != 0 && StartY != 0 && StartX != Width - 1 && StartY != Height - 1) {
 
 			int32 Direction = FMath::RandRange(0, DirectionY.Num() - 1);
 
-			StartX += DirectionX[Direction];
-			StartY += DirectionY[Direction];
-
-			if (TerrainData[StartX][StartY] != Water && TerrainData[StartX][StartY] != Grass) {
+			StartX += DirectionX[Direction] * DirectionXSign;
+			StartY += DirectionY[Direction] * DirectionYSign;
+			
+			if (TerrainData[StartX][StartY] != Grass  && TerrainData[StartX][StartY] != Water) {
 				break;
 			}
-
 			TerrainData[StartX][StartY] = Water;
 
+			riverSells.Add({ StartX ,StartY });
+
+		}
+
+		TArray<TArray<int32>> VisitedArray;
+
+		VisitedArray.SetNum(Width);
+
+		for (int32 RowIndex = 0; RowIndex < Width; ++RowIndex)
+		{
+			VisitedArray[RowIndex].SetNumZeroed(Height);
+		}
+
+		dfs_for_random_river(0, 0, VisitedArray);
+
+
+		bool is_correct = 1;
+		for (int32 i = 0; i < Width; i++)
+		{
+			for (int32 j = 0; j < Height; j++)
+			{
+				if (!VisitedArray[i][j] && TerrainData[i][j] == Grass) {
+					is_correct = 0;
+				}
+			}
+		}
+
+		if (!is_correct) {
+			for (int i = 0; i < riverSells.Num(); i++){
+				TerrainData[riverSells[i].X][riverSells[i].Y] = Grass;
+			}
 		}
 
 	}
 
+	TArray<TArray<int32>> VisitedArray;
+
+	VisitedArray.SetNum(Width);
+
+	for (int32 RowIndex = 0; RowIndex < Width; ++RowIndex)
+	{
+		VisitedArray[RowIndex].SetNumZeroed(Height);
+	}
+
+	dfs_for_random_river(0, 0, VisitedArray);
+
+
+	bool is_correct = 1;
+	for (int32 i = 0; i < Width; i++)
+	{
+		for (int32 j = 0; j < Height; j++)
+		{
+			if (!VisitedArray[i][j] && TerrainData[i][j] == Grass) {
+				is_correct = 0;
+			}
+		}
+	}
+
+	FString message = FString::Printf(TEXT("is_correct: %d"), is_correct);
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, message);
+
 
 }
-
 void GameMap::generate_enemies() {
     float x = Width * Height / 300;//one Army_position for 33^2 tiles
 
