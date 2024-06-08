a54a551d364125345035fbed96003d57ad37495a
UsatovPavel <143062791+UsatovPavel@users.noreply.github.com>
2024-06-09 01:10:22 +0300

    wrong location

--- Voevoda/BaseStrategist.cpp
+++
@@ -1,8 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "BaseStrategist.h"
-
-BaseStrategist::BaseStrategist(Location init_loc) :BaseStrategist() {
-	general.position = init_loc;
-}
